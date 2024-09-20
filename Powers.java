cedd75286bd554679570b41d55cd845542219dc0
crazyman2133 <148265659+crazyman2133@users.noreply.github.com>
2024-09-20 09:14:26 -0400

    initial commit

---
+++ Powers.java
@@ -0,0 +1,12 @@
+class Powers {
+
+    public static void main(String args[]){
+        double n = 3.5;
+
+        System.out.println("The number is = " + n);
+
+        System.out.println(n + " squared = " + n * n);
+        System.out.println(n + " cubed = " + n * n * n);
+        System.out.println(n + " to the fourted = " + Math.pow(n, 4));
+    }
+}
